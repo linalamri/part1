cc4e4ec78e70ba2ddb89146222aaf5f5667ab682
lina <lina.lamri@ensia.edu.dz>
2026-09-11 08:45:56 +0100

    updated exo2

--- exo2.py
+++ exo2.py
@@ -2,4 +2,4 @@ hat_height_cm=25
 my_height_cm=190
 #how tall am i, in meters, when wearing my hat?
 total_height_meters=(hat_height_cm+my_height_cm)/100
-print(height in meters =", total_height_meters)
+print("Height in meters =", total_height_meters)
