ef12e9c72239a62947bbfa2bd9c8befc81306b04
lina <lina.lamri@ensia.edu.dz>
2026-09-11 08:25:26 +0100

    initial commit

---
+++ exo2.py
@@ -0,0 +1,5 @@
+hat_height_cm=25
+my_height_cm=190
+#how tall am i, in meters, when wearing my hat?
+total_height_meters=(hat_height_cm+my_height_cm)/100
+print("height in meters =", total_height_meters)
